d47a4b9ec0f0f58c447949b8a2aae6666d2b7049
danx0r <ubuntu@ip-172-31-19-194.us-west-2.compute.internal>
2020-04-15 02:15:56 +0000

    more data

---
+++ datainfo.py
@@ -0,0 +1,5 @@
+import foodata_training_data
+
+print ("\nTrainig data repository:", foodata_training_data.srcpath())
+print ("\nTraining data location:", foodata_training_data.datapath())
+print ("\nContents:", foodata_training_data.list())
---
+++ training_data/private/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+def datapath():
+    i = __file__.rfind("/")
+    return __file__[:i+1]
+
+def srcpath():
+    i = __file__.rfind("/")
+    i = __file__[:i].rfind("/")
+    return __file__[:i+1]
+
+def list():
+    return os.listdir(datapath())
